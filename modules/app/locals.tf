518725f5989f328dee54bf619938f947c1e8f88b
Trent German <trentkg@gmail.com>
2026-04-01 09:50:46 -0600

    remove disabled from cloudbuild_triggers since its all manual  now

--- modules/app/locals.tf
+++ modules/app/locals.tf
@@ -3,9 +3,9 @@ locals {
   encoder_github_branch = "main"
 
   cloudbuild_triggers = {
-    transformer = { disabled = false, dockerfile_dir = "transformer" }
-    loader      = { disabled = true, dockerfile_dir = "loader" }
-    extractor   = { disabled = true, dockerfile_dir = "extractor" }
+    transformer = { dockerfile_dir = "transformer" }
+    loader      = { dockerfile_dir = "loader" }
+    extractor   = { dockerfile_dir = "extractor" }
   }
 
 }
